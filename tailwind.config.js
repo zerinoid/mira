9e12723af11987d3f0f03bd972263a0af0387d6a
zerinoid <leo.zerino@gmail.com>
2024-03-04 16:33:33 -0300

    chore: configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      red: {
+        300: "#fa5748",
+        500: "#ba141a",
+        700: "#bf0000",
+      },
+      yellow: {
+        100: "#ffffe6",
+      },
+    },
     extend: {},
   },
   plugins: [],
