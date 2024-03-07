29b05d2268b065154207122c16436d482803a433
zerinoid <leo.zerino@gmail.com>
2024-03-07 17:13:32 -0300

    feat: close icon for SliderBio

--- src/components/SliderBio.tsx
+++ src/components/SliderBio.tsx
@@ -5,15 +5,30 @@ type Props = {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 
+const CloseIcon = () => (
+  <svg
+    width="18px"
+    height="18px"
+    viewBox="0 0 512 512"
+    xmlns="http://www.w3.org/2000/svg"
+    className="fill-red-500"
+  >
+    <polygon points="427.314 107.313 404.686 84.687 256 233.373 107.314 84.687 84.686 107.313 233.373 256 84.686 404.687 107.314 427.313 256 278.627 404.686 427.313 427.314 404.687 278.627 256 427.314 107.313" />
+  </svg>
+);
+
 const SliderBio: FC<Props> = ({ isOpen, setIsOpen }) => {
   return (
     <aside
       className={`${
         isOpen ? "block" : "hidden"
-      } fixed top-0 left-0 bg-yellow-100 pl-8 pr-3 border-r border-red-500 w-[85vw] lg:w-1/5 h-screen`}
+      } fixed top-0 left-0 bg-yellow-100 pl-12 pr-3 border-r border-red-500 w-[85vw] lg:w-1/5 h-screen`}
     >
-      <header className="mb-12" onClick={() => setIsOpen((isOpen) => !isOpen)}>
-        Mira
+      <header
+        className="flex justify-between items-center mb-12"
+        onClick={() => setIsOpen((isOpen) => !isOpen)}
+      >
+        <span>Mira</span> <CloseIcon />
       </header>
       <p className="mb-12">
         Pellentesque dapibus suscipit ligula. Donec posuere augue in quam. Etiam
