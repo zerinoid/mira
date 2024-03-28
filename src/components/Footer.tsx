3d5dd4c0ea874d91a998c7134dc7f4c92d78eb8a
miraetc <leo.zerino@gmail.com>
2024-03-28 19:38:54 -0300

    feat: footer redesign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react'
 
 const Footer: FC = () => {
   return (
-    <footer className="bg-foreground text-background font-sans">
-      <div className="text-sm container flex items-center justify-center h-16">
+    <footer className="font-sans border-t">
+      <div className="text-sm text-bold container flex items-center justify-center h-12 px-[5px] md:justify-end">
         <div>
           <a
             href="mailto:leo.zerino@gmail.com"
