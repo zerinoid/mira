fd619838c9c73b39372a9025de601a8a97b2b4f6
miraetc <leo.zerino@gmail.com>
2024-03-28 14:06:20 -0300

    feat: footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,16 @@ import { FC } from 'react'
 const Footer: FC = () => {
   return (
     <footer className="bg-foreground text-background font-sans">
-      <div className="container flex items-center justify-center h-16">
-        <div>Zerino.org desenvolvimento</div>
+      <div className="text-sm container flex items-center justify-center h-16">
+        <div>
+          <a
+            href="mailto:leo.zerino@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            zerino desenvolvimento
+          </a>
+        </div>
       </div>
     </footer>
   )
