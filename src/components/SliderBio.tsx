345ba24794d4c9682464eaf14930e472dcd6a492
miraetc <leo.zerino@gmail.com>
2024-04-17 16:11:28 -0300

    feat: close icon supressed

--- src/components/SliderBio.tsx
+++ src/components/SliderBio.tsx
@@ -35,18 +35,6 @@ interface Bio {
   bio: string
 }
 
-const CloseIcon = () => (
-  <svg
-    width="20px"
-    height="20px"
-    viewBox="0 0 512 512"
-    xmlns="http://www.w3.org/2000/svg"
-    className="fill-background"
-  >
-    <polygon points="427.314 107.313 404.686 84.687 256 233.373 107.314 84.687 84.686 107.313 233.373 256 84.686 404.687 107.314 427.313 256 278.627 404.686 427.313 427.314 404.687 278.627 256 427.314 107.313" />
-  </svg>
-)
-
 const SliderBio: FC<Props> = ({ userId, isOpen, setIsOpen }) => {
   const [bioText, setBioText] = useState<string>('')
   const [isEditingBio, setIsEditingBio] = useState<boolean>(false)
@@ -133,7 +121,6 @@ const SliderBio: FC<Props> = ({ userId, isOpen, setIsOpen }) => {
           <div className="w-[20px]">
             <MiraLogo className="fill-background" />
           </div>
-          {/* <CloseIcon /> */}
         </header>
         {isEditingBio ? (
           <Form {...form}>
